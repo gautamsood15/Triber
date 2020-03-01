b8a1792e6134e9b1b113ab4f9a30fd8e723946b7
gautamsood15 <gauty22@gmail.com>
2020-03-02 01:15:10 +0530

    update view FXN added

--- TriberVenv/src/tweets/mixins.py
+++ TriberVenv/src/tweets/mixins.py
@@ -6,10 +6,19 @@ class FormUserNeededMixin(object):
 
 		def form_valid(self, form):
 			if self.request.user.is_authenticated():
-
 				form.instance.user = self.request.user
 				return super(FormUserNeededMixin, self).form_valid(form)
 
 			else:
 				form._errors[forms.forms.NON_FIELD_ERRORS] = ErrorList(["User must be logged in to continue"])
+				return self.form_invalid(form)
+
+
+class UserOwnerMixin(object):
+	def form_valid(self, form):
+			if form.instance.user == self.request.user:
+				return super(FormUserNeededMixin, self).form_valid(form)
+
+			else:
+				form._errors[forms.forms.NON_FIELD_ERRORS] = ErrorList(["This user is not allowed to chnage this data."])
 				return self.form_invalid(form)
--- TriberVenv/src/tweets/views.py
+++ TriberVenv/src/tweets/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView, ListView, CreateView
+from django.views.generic import DetailView, ListView, CreateView, UpdateView
 
 from .forms import TweetModelForm
-from .mixins import FormUserNeededMixin
+from .mixins import FormUserNeededMixin, UserOwnerMixin
 from .models import Tweet
 
 
@@ -13,12 +13,21 @@ from .models import Tweet
 
 # CRUD List/Search
 
-class TweetCreateView(LoginRequiredMixin, FormUserNeededMixin, CreateView):
+class TweetCreateView(FormUserNeededMixin, CreateView):
 	form_class = TweetModelForm
 	template_name = 'tweets/create_view.html'
 	success_url = "/tweet/create"
-	login_url = '/admin/'
+	#login_url = '/admin/'
 	
+
+
+class TweetUpdateView(UserOwnerMixin, UpdateView):
+	queryset = Tweet.objects.all()
+	form_class = TweetModelForm
+	template_name = 'tweets/update_view.html'
+	success_url = "/tweet/"
+
+
 
 	#def form_valid(self, form):
 	#	if self.request.user.is_authenticated():
