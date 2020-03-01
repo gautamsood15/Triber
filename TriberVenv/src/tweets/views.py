5887adf76a600e90749454deee9f4bacdee33cb7
gautamsood15 <gauty22@gmail.com>
2020-03-01 19:58:22 +0530

    Adding dynamic URL Routing

--- TriberVenv/src/tweets/views.py
+++ TriberVenv/src/tweets/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView, ListView
 from .models import Tweet
 
@@ -13,7 +13,10 @@ class TweetDetailView(DetailView):
 	queryset = Tweet.objects.all()
 
 	def get_object(self):
-		return Tweet.objects.get(id=1)
+		print(self.kwargs)
+		pk = self.kwargs.get("pk")
+		obj = get_object_or_404(Tweet, pk=pk)
+		return obj
 
 class TweetListView(ListView):
 	#template_name = "tweets/list_view.html"
