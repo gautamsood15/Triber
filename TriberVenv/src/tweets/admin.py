365f39f9f1979b418e7b45b402349065672e0ee8
gautamsood15 <gauty22@gmail.com>
2020-03-01 23:33:37 +0530

    Adding create View FXN

--- TriberVenv/src/tweets/admin.py
+++ TriberVenv/src/tweets/admin.py
@@ -8,9 +8,9 @@ admin.site.register(Tweet)
 
 
 class TweetModelAdmin(admin.ModelAdmin):
-	form = TweetModelForm
-	#class Meta:
-	#	model = Tweet
+	#form = TweetModelForm
+	class Meta:
+		model = Tweet
 		
 admin.site.unregister(Tweet)
 admin.site.register(Tweet, TweetModelAdmin)
--- TriberVenv/src/tweets/views.py
+++ TriberVenv/src/tweets/views.py
@@ -1,11 +1,54 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, CreateView
+
+from .forms import TweetModelForm
 from .models import Tweet
+
 
 # Create your views here.
 
 
 # CRUD List/Search
+
+class TweetCreateView(CreateView):
+	template_name = 'tweets/create_view.html'
+	success_url = "/tweet/create"
+	form_class = TweetModelForm
+	def form_valid(self, form):
+		form.instance.user = self.request.user
+		return super(TweetCreateView, self).form_valid(form)
+
+
+
+#def tweet_create_view(request):
+#	form = TweetModelForm(request.POST or None):
+#
+#	if form.is_valid():
+#		instance = form.save(commit=False)
+#		instance.user = request.user
+#		instance.save()
+#
+#	context = {
+#			"form":form
+#		}
+#
+#	return render(request, 'tweets/create_view.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 class TweetDetailView(DetailView):
