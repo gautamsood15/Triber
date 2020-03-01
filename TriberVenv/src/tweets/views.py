b438b1b12c4e387f5feaaa98211a0d5deff567cd
gautamsood15 <gauty22@gmail.com>
2020-03-01 15:00:16 +0530

    adding context to the templeated and rendering

--- TriberVenv/src/tweets/views.py
+++ TriberVenv/src/tweets/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from .models import Tweet
+
 # Create your views here.
 
 
@@ -7,7 +9,24 @@ from django.shortcuts import render
 
 
 def tweet_detail_view(request, id=1):
-	return render(request, "tweets/detail_view.html", {})
+	obj = Tweet.objects.get(id=id)     # GET from database 1 item
+	print(obj)
+	context = {
+		"object": obj
+	}
+
+	return render(request, "tweets/detail_view.html", context)
+
+
+
 
 def tweet_list_view(request):
-	return render(request, "tweets/list_view.html", {})
+	queryset = Tweet.objects.all()    #show  all  items in the database
+	print(queryset)
+	for obj in queryset:
+		print(obj.content)
+	context = {
+		"object_list": queryset
+	}
+
+	return render(request, "tweets/list_view.html", context)
