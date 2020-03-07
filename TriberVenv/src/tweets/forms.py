cfbf9a716416bab42cfd9f16bd834b0c3a491ccf
gautamsood15 <gauty22@gmail.com>
2020-03-08 02:18:03 +0530

    changing crispy forms 1.6.1

--- TriberVenv/src/tweets/forms.py
+++ TriberVenv/src/tweets/forms.py
@@ -3,11 +3,11 @@ from django.core.exceptions import ValidationError
 from .models import Tweet
 
 class TweetModelForm(forms.ModelForm):
-	content = forms.CharField(label='', 
-		widget=forms.Textarea(
-			attrs={'placeholder':"Your Message", 
-			"class":"form-control"}
-			))
+#	content = forms.CharField(label='', 
+#		widget=forms.Textarea(
+#			attrs={'placeholder':"Your Message", 
+#			"class":"form-control"}
+#			))
 	
 	class Meta:
 		model = Tweet
