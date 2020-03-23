043b37cde656917bd99645dec5bac64fae291166
gautamsood15 <gauty22@gmail.com>
2020-03-23 08:52:12 +0530

    Retweet complete

--- TriberVenv/src/tweets/api/urls.py
+++ TriberVenv/src/tweets/api/urls.py
@@ -3,13 +3,14 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView
 
 # from .views import  TweetDetailView, TweetDeleteView, TweetListView, TweetCreateView, TweetUpdateView
-from .views import TweetCreateAPIView,TweetListAPIView
+from .views import RetweetAPIView,TweetCreateAPIView,TweetListAPIView
 
 
 urlpatterns = [
 #	url(r'^$', RedirectView.as_view(url="/")),
     url(r'^$', TweetListAPIView.as_view(), name='list'),
     url(r'^create/$', TweetCreateAPIView.as_view(), name='create'),
+    url(r'^(?P<pk>\d+)/retweet/$', RetweetAPIView.as_view(), name='retweet'),
 #   url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'),
 #   url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),
 #   url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name='delete'),
